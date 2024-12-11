1c7966d66922b1289b2d4550d89a552cc5968555
mutia <mutiashl76@gmail.com>
2024-12-11 23:28:09 +0700

    mutia mengubah file

--- codeigniter4-framework-2849e7f/app/Controllers/entriprofil.php
+++ codeigniter4-framework-2849e7f/app/Controllers/entriprofil.php
@@ -1,4 +1,5 @@
 <?php
+
 class Profile extends CI_Controller {
 
     public function __construct()
--- codeigniter4-framework-2849e7f/app/Controllers/evaluasi.php
+++ codeigniter4-framework-2849e7f/app/Controllers/evaluasi.php
@@ -1,4 +1,5 @@
 <?php
+
 class Evaluation extends CI_Controller {
 
     public function __construct()
