b216371af147009e9a3e867fb272fccf37758a88
mutia <mutiashl76@gmail.com>
2024-12-11 22:48:03 +0700

    mutia mengubah evaluasi.php

---
+++ codeigniter4-framework-2849e7f/app/Controllers/evaluasi.php
@@ -0,0 +1,40 @@
+<?php
+class Evaluation extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('Evaluation_model');
+        $this->load->helper(array('form', 'url'));
+    }
+
+    public function index()
+    {
+        $this->load->view('evaluation_form');
+    }
+
+    public function submit()
+    {
+        $this->load->library('form_validation');
+
+        $this->form_validation->set_rules('nama', 'Nama', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
+        $this->form_validation->set_rules('kritik_saran', 'Kritik & Saran', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('evaluation_form');
+        } else {
+            $data = array(
+                'nama' => $this->input->post('nama'),
+                'email' => $this->input->post('email'),
+                'kritik_saran' => $this->input->post('kritik_saran')
+            );
+
+            if ($this->Evaluation_model->save_evaluation($data)) {
+                $this->load->view('submission_success');
+            } else {
+                $this->load->view('submission_failure');
+            }
+        }
+    }
+}
